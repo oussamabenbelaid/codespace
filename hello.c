51ceafdd9e3efd4419d55c6fbfbb517fce6e5c3a
oussamabenbelaid <122389102+oussamabenbelaid@users.noreply.github.com>
2023-01-11 18:35:47 +0000

    Wed, Jan 11, 2023, 6:35 PM +00:00

--- hello.c
+++ hello.c
@@ -4,7 +4,7 @@ int main(void){
     int num1 = get_int("enter a number here:");
     int num2 = get_int("num2:");
     int result = enter a number here + num2;
-    printf("%i\n", result);
+    printf("%i\n", result );
 
 
 }
