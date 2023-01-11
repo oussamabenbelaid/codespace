e5d4d51799a5a5a5937115a0c05c65570096aa7c
oussamabenbelaid <122389102+oussamabenbelaid@users.noreply.github.com>
2023-01-11 18:41:35 +0000

    Wed, Jan 11, 2023, 6:41 PM +00:00

--- hello.c
+++ hello.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include <cs50.h>
 int main(void){
-    int num1 = get_int("num1:");
-    int num2 = get_int("num2:");
-    int result = num1 + num2;
-    printf("%i\n", result );
+    long num1 = get_long("num1:");
+    long num2 = get_long("num2:");
+    
+    printf("%li\n", num1 + num2 );
 
 
 }
