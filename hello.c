068b1ac88ee6e9726d5c18a0a578ce544720b7ca
oussamabenbelaid <122389102+oussamabenbelaid@users.noreply.github.com>
2023-01-12 20:03:29 +0000

    Thu, Jan 12, 2023, 8:03 PM +00:00

--- hello.c
+++ hello.c
@@ -1,13 +1,6 @@
 #include <stdio.h>
 #include <cs50.h>
 int main(void){
-    int x = get_int("x: ");
-    int y = get_int("y: ");
-    if(x < y){
-        printf("x is less than y\n");
-    }
-    else{
-        printf("x is not less than y\n");
-    }
+ 
 
 }
